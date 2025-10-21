3c4e11493ef75c0550bfa62865fc0c31c21b178a
TimTheBig <132001783+TimTheBig@users.noreply.github.com>
2025-10-21 14:42:45 -0400

    Improve tests

--- src/tests.rs
+++ src/tests.rs
@@ -7,7 +7,11 @@ fn test_bindings_ext() {
     init_build_logger();
 
     build_shader_dir(
-        "./test/src/shader",
+        "./test/src/shaders",
         &mut [WgpuBindingsExtension::new("./test/src/shader_bindings").unwrap()]
-    ).unwrap()
+    ).unwrap();
+
+    assert!(std::fs::exists("test/src/shaders/test.wgsl").unwrap(), "test_bindings_ext requires shaders/test.wgsl to exist");
+    assert!(std::fs::exists("test/src/shader_bindings/test.rs").unwrap(), "no shader_binding was generated for test.wgsl");
+    assert!(std::fs::exists("test/src/shader_bindings/mod.rs").unwrap(), "no mod.rs was generated for shader_bindings root mod");
 }
